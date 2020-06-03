b0f065526555b26e43e6f55eafb188f47f19c7f0
shreeviknesh <luckyboy1998@gmail.com>
2020-06-03 16:23:41 +0530

    Added .clang-format

--- Complex.h
+++ Complex.h
@@ -4,56 +4,57 @@
 #ifndef COMPLEX_H
 #define COMPLEX_H
 
-class Complex {
-private:
-	double a;
-	double b;
+class Complex
+{
+  private:
+    double a;
+    double b;
 
-public:
-	// Constructors & Desctructor
-	Complex() noexcept;
-	Complex(double, double) noexcept;
-	Complex(const Complex&) noexcept;
+  public:
+    // Constructors & Desctructor
+    Complex() noexcept;
+    Complex(double, double) noexcept;
+    Complex(const Complex&) noexcept;
 
-	inline double real() const noexcept { return a; }
-	inline double imag() const noexcept { return b; }
+    inline double real() const noexcept { return a; }
+    inline double imag() const noexcept { return b; }
 
-	friend double real(const Complex&) noexcept;
-	friend double imag(const Complex&) noexcept;
+    friend double real(const Complex&) noexcept;
+    friend double imag(const Complex&) noexcept;
 
-	// Assignment operator
-	Complex& operator = (const Complex&) noexcept;
+    // Assignment operator
+    Complex& operator=(const Complex&) noexcept;
 
-	// Binary arithmetic operators
-	Complex& operator += (const Complex&) noexcept;
-	Complex& operator -= (const Complex&) noexcept;
-	Complex& operator *= (const Complex&) noexcept;
-	Complex& operator /= (const Complex&);
-	
-	// Arithmetic
-	friend Complex operator + (const Complex&, const Complex&) noexcept;
-	friend Complex operator - (const Complex&, const Complex&) noexcept;
-	friend Complex operator * (const Complex&, const Complex&) noexcept;
-	friend Complex operator / (const Complex&, const Complex&);
+    // Binary arithmetic operators
+    Complex& operator+=(const Complex&) noexcept;
+    Complex& operator-=(const Complex&) noexcept;
+    Complex& operator*=(const Complex&) noexcept;
+    Complex& operator/=(const Complex&);
 
-	// Logical
-	friend bool operator == (const Complex&, const Complex&) noexcept;
-	friend bool operator != (const Complex&, const Complex&) noexcept;
+    // Arithmetic
+    friend Complex operator+(const Complex&, const Complex&) noexcept;
+    friend Complex operator-(const Complex&, const Complex&) noexcept;
+    friend Complex operator*(const Complex&, const Complex&) noexcept;
+    friend Complex operator/(const Complex&, const Complex&);
 
-	// Absolute value
-	double abs() const noexcept;
-	friend double abs(const Complex&) noexcept;
+    // Logical
+    friend bool operator==(const Complex&, const Complex&) noexcept;
+    friend bool operator!=(const Complex&, const Complex&) noexcept;
 
-	// Argument value
-	double arg() const noexcept;
-	friend double arg(const Complex&) noexcept;
+    // Absolute value
+    double abs() const noexcept;
+    friend double abs(const Complex&) noexcept;
 
-	// Conjugate value
-	Complex conj() const noexcept;
-	friend Complex conj(const Complex&) noexcept;
+    // Argument value
+    double arg() const noexcept;
+    friend double arg(const Complex&) noexcept;
 
-	// Output
-	friend std::ostream& operator<<(std::ostream&, const Complex&);
+    // Conjugate value
+    Complex conj() const noexcept;
+    friend Complex conj(const Complex&) noexcept;
+
+    // Output
+    friend std::ostream& operator<<(std::ostream&, const Complex&);
 };
 
 #endif
